63761d211f22d2c4b5e8dc1930259c9694edc8b0
michellealoya <122331663+michellealoya@users.noreply.github.com>
2024-09-17 20:38:18 -0600

    Project0

---
+++ Project0/Template.asm
@@ -0,0 +1,30 @@
+TITLE Program Template     (template.asm)
+
+; Author: Michelle Loya
+; Last Modified: 2023
+; OSU email address: loyami@oregonstate.edu
+; Course number/section: CS271 Section 400
+; Project Number:                 Due Date:
+; Description: 
+
+INCLUDE Irvine32.inc
+
+; (insert macro definitions here)
+
+; (insert constant definitions here)
+
+.data
+
+; (insert variable definitions here)
+
+.code
+main PROC
+
+; (insert executable instructions here)
+
+	Invoke ExitProcess,0	; exit to operating system
+main ENDP
+
+; (insert additional procedures here)
+
+END main
